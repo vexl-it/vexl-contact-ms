ea00154a74bc5652ab278cbc19cdf88c1382475a
David Tilšer <davidtilser@gmail.com>
2022-02-13 19:39:47 +0100

    Added missing uncommitted files

---
+++ src/main/java/com/cleevio/vexl/common/dto/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.cleevio.vexl.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private Collection<String> message;
+
+    private String code;
+
+}
