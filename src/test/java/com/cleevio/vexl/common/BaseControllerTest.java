92eac28186a9a0fbcd2ea51109dfc7ad94163a59
David Tilšer <davidtilser@gmail.com>
2022-02-17 16:30:38 +0100

    Prepared environment for tests

---
+++ src/test/java/com/cleevio/vexl/common/BaseControllerTest.java
@@ -0,0 +1,42 @@
+package com.cleevio.vexl.common;
+
+import com.cleevio.vexl.module.contact.service.ContactService;
+import com.cleevio.vexl.module.contact.service.FacebookService;
+import com.cleevio.vexl.module.contact.service.ImportService;
+import com.cleevio.vexl.module.user.entity.User;
+import com.cleevio.vexl.module.user.service.UserService;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class BaseControllerTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @MockBean
+    protected UserService userService;
+
+    @MockBean
+    protected ContactService contactService;
+
+    @MockBean
+    protected FacebookService facebookService;
+
+    @MockBean
+    protected ImportService importService;
+
+    @Mock
+    protected User user;
+
+
+    @BeforeEach
+    @SneakyThrows
+    public void setup() {
+
+
+    }
+}
