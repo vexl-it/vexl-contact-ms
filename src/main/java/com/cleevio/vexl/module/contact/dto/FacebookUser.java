2beb613c372e70fc3701abf0636765e6e35babbe
David Tilšer <davidtilser@gmail.com>
2022-02-28 16:26:55 +0100

    Adjust FacebookUser for our needs - we will have in response only facebookId, name and friends.

--- src/main/java/com/cleevio/vexl/module/contact/dto/FacebookUser.java
+++ src/main/java/com/cleevio/vexl/module/contact/dto/FacebookUser.java
@@ -1,17 +1,25 @@
 package com.cleevio.vexl.module.contact.dto;
 
 import com.restfb.Facebook;
-import com.restfb.types.User;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Collections.unmodifiableList;
 
-public class FacebookUser extends User {
+@Data
+public class FacebookUser {
+
+    @Facebook
+    private String id;
+
+    @Facebook
+    private String name;
 
     @Facebook("friends")
     private final List<FacebookUser> friends = new ArrayList<>();
+
     private final List<FacebookUser> newFriends = new ArrayList<>();
 
     public List<FacebookUser> getFriends() {
