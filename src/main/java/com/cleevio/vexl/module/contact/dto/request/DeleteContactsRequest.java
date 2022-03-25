68fd804a706fb0e4130506e876e075af4e1e08f1
David Tilšer <davidtilser@gmail.com>
2022-03-25 14:43:31 +0100

    Fixed constrain for list of deletedContacts

--- src/main/java/com/cleevio/vexl/module/contact/dto/request/DeleteContactsRequest.java
+++ src/main/java/com/cleevio/vexl/module/contact/dto/request/DeleteContactsRequest.java
@@ -3,13 +3,13 @@ package com.cleevio.vexl.module.contact.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class DeleteContactsRequest {
 
-    @NotBlank
+    @NotNull
     @Schema(required = true, description = "Public keys of contacts to delete in Base64 format")
     private List<String> contactsToDelete;
 }
